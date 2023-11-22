9479d8a661144b4125ea528cd2c921dfd71b005e
Kiran Deep <kiran.shine3@gmail.com>
2023-11-22 20:50:28 +0530

    update

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ let rootEl = document.getElementById("root");
 
 let imgTagEl = document.getElementById("imgTag");
 
+const loaderEl = document.getElementById("loader");
+
 function fetchApiData(data) {
 
     let resultContainer = document.createElement("div");
     rootEl.appendChild(resultContainer);
 
-    // let userNameEl = document.createElement("h1");
-    // userNameEl.textContent = data.data.full_name;
-    // userNameEl.classList.add("user-name");
-    // resultContainer.appendChild(userNameEl);
+    let userNameEl = document.createElement("h1");
+    userNameEl.textContent =  data.data.full_name;
+    userNameEl.classList.add("user-name");
+    resultContainer.appendChild(userNameEl);
 
-    // let breakTag = document.createElement("br");
-    // userNameEl.appendChild(breakTag);
+    const bio = data.data.biography;
+
+    let array = bio.split('/[\n,\n\n]/',1);
+    console.log(array);
+
+
+    let bioEl = document.createElement("h1");
+    bioEl.textContent = array[0];
+    bioEl.classList.add("user-name");
+    resultContainer.appendChild(bioEl);
 
     let profileLink1 = document.createElement("a");
     profileLink1.href = data.data.hd_profile_pic_url_info.url;
@@ -45,10 +55,6 @@ function fetchApiData(data) {
     profileLink3.classList.add("links-style");
     resultContainer.appendChild(profileLink3);
 
-    // let imageEl = document.createElement("img");
-    // imageEl.src = data.data.hd_profile_pic_url_info.url;
-    // imageEl.classList.add("image-size","d-flex","flex-row","justify-content-center");
-    // resultContainer.appendChild(imageEl);
 
     let profileContainerEl = document.getElementById("profileContainer");
 
@@ -57,15 +63,105 @@ function fetchApiData(data) {
     profilePicEl.classList.add("user-image");
     profileContainerEl.appendChild(profilePicEl);
 
-    let userNameEl = document.createElement("h1");
-    userNameEl.textContent = "Hello !" + "    " + data.data.full_name;
-    userNameEl.classList.add("user-name");
-    profileContainerEl.appendChild(userNameEl);
+    // let usernameContainerEl = document.createElement("div");
+    // resultContainer.appendChild(usernameContainerEl);
+
+    // let userNameEl = document.createElement("h1");
+    // userNameEl.textContent =  data.data.full_name;
+    // userNameEl.classList.add("user-name");
+    // usernameContainerEl.appendChild(userNameEl);
+
+
+    const number = data.data.follower_count;
+    const formattedNumber = formatNumber(number);
+    console.log(formattedNumber);
+
+    const number2 = data.data.media_count;
+    const postedNumber = postNumber(number2);
+    console.log(postedNumber);
+
+    const number3 = data.data.following_count;
+    const followingNumber = followingCount(number3);
+    console.log(followingNumber);
+
+
+    let postsCountEl = document.createElement("h1");
+    postsCountEl.textContent = postedNumber;
+    postsCountEl.classList.add("posts-count", "text-center");
+    profileContainerEl.appendChild(postsCountEl);
+
+    let postEl = document.createElement("h1");
+    postEl.textContent = "Posts";
+    postEl.classList.add("postname");
+    postsCountEl.appendChild(postEl);
+
+    let followersCountEl = document.createElement("h1");
+    followersCountEl.textContent = formattedNumber;
+    followersCountEl.classList.add("posts-count", "text-center");
+    profileContainerEl.appendChild(followersCountEl);
+
+    let followersEl = document.createElement("h1");
+    followersEl.textContent = "Followers";
+    followersEl.classList.add("postname");
+    followersCountEl.appendChild(followersEl);
+    
+    let followingCountEl = document.createElement("h1");
+    followingCountEl.textContent = followingNumber;
+    followingCountEl.classList.add("posts-count", "text-center");
+    profileContainerEl.appendChild(followingCountEl);
+
+    let followingEl = document.createElement("h1");
+    followingEl.textContent = "Following";
+    followingEl.classList.add("postname");
+    followingCountEl.appendChild(followingEl);
+
+    loaderEl.classList.add("d-none");
 
 }
 
-function fetchingApi (username_or_id_or_url) {
+function formatNumber(number) {
+    if (number >= 1000) {
+      const suffixes = ["", "k", "M", "B", "T"];
+      const suffixNum = Math.floor(("" + number).length / 4);
+      let shortValue = parseFloat((suffixNum !== 0 ? number / Math.pow(1000, suffixNum) : number).toPrecision(2));
+      if (shortValue % 1 !== 0) {
+        shortValue = shortValue.toFixed(1);
+      }
+      return shortValue + suffixes[suffixNum];
+    }
+    return number;
+  }
+  
+function postNumber(number2) {
+    if (number2 >= 1000) {
+      const suffixes = ["", "k", "M", "B", "T"];
+      const suffixNum = Math.floor(("" + number2).length / 3);
+      let shortValue = parseFloat((suffixNum !== 0 ? number2 / Math.pow(1000, suffixNum) : number2).toPrecision(2));
+      if (shortValue % 1 !== 0) {
+        shortValue = shortValue.toFixed(1);
+      }
+      return shortValue + suffixes[suffixNum];
+    }
+    return number2;
+  }
 
+function followingCount(number3) {
+    if (number3 >= 1000) {
+      const suffixes = ["", "k", "M", "B", "T"];
+      const suffixNum = Math.floor(("" + number3).length / 3);
+      let shortValue = parseFloat((suffixNum !== 0 ? number3 / Math.pow(1000, suffixNum) : number3).toPrecision(2));
+      if (shortValue % 1 !== 0) {
+        shortValue = shortValue.toFixed(1);
+      }
+      return shortValue + suffixes[suffixNum];
+    }
+    return number3;
+  }
+
+  // Example usage:
+
+
+function fetchingApi (username_or_id_or_url) {
 
         let key = '80e004b79fmsh93c81eaada8f74bp107d7bjsn1f7e1d3a6074';
         let url = 'https://instagram-scraper-api2.p.rapidapi.com/v1/info?&url_embed_safe=true&username_or_id_or_url=';
@@ -86,6 +182,7 @@ function fetchingApi (username_or_id_or_url) {
             console.log(data);
             fetchApiData(data);
             }) 
+        loaderEl.classList.add("d-none");
 }
 
 
@@ -96,6 +193,6 @@ function submitButton () {
     else {
         fetchingApi(seacrhInputEl.value);
         seacrhInputEl.value = "";
-
+        loaderEl.classList.remove("d-none");
     }
 }
